f64580df774c924e82db912136627b19191a906d
DanielZamperlini <monkeydluffysan2021@gmail.com>
2022-04-02 22:53:48 -0300

    bug Lf

--- main.js
+++ main.js
@@ -41,7 +41,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 
 /***/ }),
 
-/***/ "./js/modules/dropdown-menu.js":
+/***/ './js/modules/dropdown-menu.js':
 /*!*************************************!*\
   !*** ./js/modules/dropdown-menu.js ***!
   \*************************************/
